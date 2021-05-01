90987e80fd049d1ff3e12436de1625c79ae67368
Hrishikesh D Kakkad <hrishidkakkad@gmail.com>
2021-05-01 07:50:14 +0530

    Firebase setup - and test request

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,9 +1,36 @@
 import 'dotenv/config';
-const { PORT, NODE_ENV } = process.env;
+const {
+  PORT,
+  NODE_ENV,
+  PRIVATE_KEY,
+  FIRESTORE_TYPE,
+  FIRESTORE_PROJECT_ID,
+  FIRESTORE_PRIVATE_KEY_ID,
+  FIRESTORE_CLIENT_EMAIL,
+  FIRESTORE_CLIENT_ID,
+  FIRESTORE_AUTH_URI,
+  FIRESTORE_TOKEN_URI,
+  FIRESTORE_AUTH_PROVIDER_CERT_URL,
+  FIRESTORE_CLIENT_CERT_URL,
+} = process.env;
+
+console.log(PRIVATE_KEY.replace(/\\n/g, '\n'));
 
 export const config = {
   application: {
     PORT: PORT,
     environment: NODE_ENV,
+  },
+  DB: {
+    type: FIRESTORE_TYPE,
+    project_id: FIRESTORE_PROJECT_ID,
+    private_key_id: FIRESTORE_PRIVATE_KEY_ID,
+    private_key: PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: FIRESTORE_CLIENT_EMAIL,
+    client_id: FIRESTORE_CLIENT_ID,
+    auth_uri: FIRESTORE_AUTH_URI,
+    token_uri: FIRESTORE_TOKEN_URI,
+    auth_provider_x509_cert_url: FIRESTORE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: FIRESTORE_CLIENT_CERT_URL,
   },
 };
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,14 @@
-export const dbConnection = {
-  url: `Some URL`,
+import * as admin from 'firebase-admin';
+import path from 'path';
+import { config } from '../config/app.config';
+
+const serviceAccount = path.resolve(__dirname, '../../oota-cbff3-firebase-adminsdk-fil0x-881873ff3d.json');
+
+export const dbConfig = () => {
+  admin.initializeApp({
+    credential: admin.credential.cert(config.DB),
+    databaseURL: 'https://oota-debug.firebaseio.com',
+  });
+  const db = admin.firestore();
+  return db;
 };
