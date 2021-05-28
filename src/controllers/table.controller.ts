20821259a590fdb5b1edb8d3dd942cf081cfbe99
Hrishikesh Kakkad <hrishidkakkad@outlook.com>
2021-05-28 23:55:53 +0530

    COMPLETE - VERIFY API - 2

--- src/controllers/table.controller.ts
+++ src/controllers/table.controller.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import HttpException from '../exceptions/HttpException';
 import { IStatus, ITableActivateBody } from '../interfaces/common.interface';
 import { tableService } from '../services/RestaurantService/TableService/table.service';
 
