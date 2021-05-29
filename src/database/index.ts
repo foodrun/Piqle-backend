78880c0144fc35ee794c9b667f9a9e02e28c8be6
Hrishikesh Kakkad <hrishidkakkad@outlook.com>
2021-05-29 10:13:39 +0530

    COMPLETE - VERIFY API - added debug log 6

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,18 +2,13 @@ import * as admin from 'firebase-admin';
 import { config } from '../config/app.config';
 
 export const initializeAdmin = () => {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(config.DB as admin.ServiceAccount),
-      databaseURL: config.DB_URL,
-    });
-  } catch (error) {
-    console.log(error, 'init error');
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert(config.DB as admin.ServiceAccount),
+    databaseURL: config.DB_URL,
+  });
 };
 
 export const dbConfig = () => {
   const db = admin.firestore();
-  console.log(db, 'dbs');
   return db;
 };
