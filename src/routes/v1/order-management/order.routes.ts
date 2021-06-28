27cb35875325271a4de7af5c4ffb4a59d6fe430f
Hrishikesh Kakkad <hrishidkakkad@outlook.com>
2021-06-28 19:05:18 +0530

    Update order status

--- src/routes/v1/order-management/order.routes.ts
+++ src/routes/v1/order-management/order.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { orderController } from '../../../controllers/order.controller';
 import { GAuth } from '../../../middlewares/auth/google/auth.middleware';
 import { CreateOrderValidator } from '../../../middlewares/validator/create-order.validator';
+import { UpdateOrderValidator } from '../../../middlewares/validator/update-order-status.validator';
 
 const orderRouter = Router();
 
@@ -11,5 +12,7 @@ orderRouter.post(
   CreateOrderValidator,
   orderController.createNewTableOrder,
 );
+
+orderRouter.post('/order-management/update-order', UpdateOrderValidator, orderController.updateOrderStatus);
 
 export default orderRouter;
