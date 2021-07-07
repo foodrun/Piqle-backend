6389d26b9bc6d8241d467f25493a1182cbc23110
Hrishikesh Kakkad <hrishidkakkad@outlook.com>
2021-07-08 00:32:20 +0530

    Completed order update service

---
+++ src/services/RestaurantService/ItemsService/menu-items-update.service.ts
@@ -0,0 +1,35 @@
+import HttpException from '../../../exceptions/HttpException';
+import { IMenu, IMenuUpdate } from '../../../interfaces/menu-items.interface';
+import lodash from 'lodash';
+export class ItemsUpdateService {
+  constructor(private _menuItemToUpdate: IMenuUpdate) {}
+  private _menuData: IMenu;
+  private _updatedMenuData: IMenu;
+
+  setMenuData(data: IMenu) {
+    // Call when we fetch menu object from DB
+    this._menuData = data;
+  }
+
+  async processNewItemIntoCurrentObject() {
+    const newItem = this._menuItemToUpdate;
+    delete newItem.type;
+    const currentMenu = this._menuData;
+    // check to see if the item exists array of item objects through item ID
+    const doesItemToUpdateExistInCurrentMenu = this.checkIfItemExistsInCurrentMenu();
+    if (!doesItemToUpdateExistInCurrentMenu)
+      throw new HttpException(400, 'Item to update does not exist in current menu');
+    const items = currentMenu.items;
+    const index = items.findIndex(item => item.id === newItem.id);
+    this._updatedMenuData = lodash.cloneDeep(currentMenu);
+    this._updatedMenuData.items[index] = newItem;
+  }
+
+  getUpdatedMenuData(): IMenu {
+    return this._updatedMenuData;
+  }
+
+  checkIfItemExistsInCurrentMenu(): boolean {
+    return !!this._menuData.items.find(item => item.id === this._menuItemToUpdate.id);
+  }
+}
--- src/tests/ItemsService/UpdateMenuItem/updateItem.test.ts
+++ src/tests/ItemsService/UpdateMenuItem/updateItem.test.ts
@@ -1,11 +1,11 @@
-import { ItemsService } from '../../../services/RestaurantService/ItemsService/items.service';
+import { ItemsUpdateService } from '../../../services/RestaurantService/ItemsService/menu-items-update.service';
 import { menuItem } from './menuItem.constant';
 import { UpdateItem } from './updateItem.constant';
 import { updatedItemInMenu } from './updatedItem.constant';
 
 describe('If the object in the array is updating rightly', () => {
   it('Updates the item with customizations rightly', () => {
-    const itemService = new ItemsService(UpdateItem);
+    const itemService = new ItemsUpdateService(UpdateItem);
     itemService.setMenuData(menuItem);
     itemService.processNewItemIntoCurrentObject();
     expect(itemService.getUpdatedMenuData()).toEqual(updatedItemInMenu);
