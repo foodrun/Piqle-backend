0eea20a524d2fb49a47b67fe599604af631b8016
Hrishikesh Kakkad <hrishidkakkad@outlook.com>
2021-07-13 07:37:25 +0530

    Completed order revamp

--- src/services/RestaurantService/OrderService/OrderOperations/order-ops.service.ts
+++ src/services/RestaurantService/OrderService/OrderOperations/order-ops.service.ts
@@ -8,12 +8,12 @@ import { ORDERS, RESTAURANTS, SESSIONS, TABLES } from '../../../../constants';
 import { dbConfig } from '../../../../database';
 import { OrderStatus } from '../../../../enums/orderStatus.enum';
 import { IUser } from '../../../../interfaces/common.interface';
-import { IOrder } from '../../../../interfaces/order.interface';
+import { INewOrder, IOrder } from '../../../../interfaces/order.interface';
 import { IOrderBillDetails } from '../../../../interfaces/orderBill.interface';
 
 interface IOrderClass {
   createNewOrder(
-    orderDetails: IOrder,
+    orderDetails: INewOrder,
     userDetails: IUser,
     orderBillDetails: IOrderBillDetails,
   ): Promise<FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>>;
@@ -21,9 +21,10 @@ interface IOrderClass {
 
 class OrderOperations implements IOrderClass {
   async createNewOrder(
-    orderDetails: IOrder,
+    orderDetails: INewOrder,
     userDetails: IUser,
-    orderBillDetails: IOrderBillDetails,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    orderBillDetails?: IOrderBillDetails,
   ): Promise<FirebaseFirestore.DocumentReference<FirebaseFirestore.DocumentData>> {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const { tableID, sessionID, restaurantID, ...order } = orderDetails;
@@ -35,7 +36,7 @@ class OrderOperations implements IOrderClass {
         orderStatus: OrderStatus.PLACED,
         ...order,
         ...userDetails,
-        orderBillDetails,
+        // orderBillDetails,
         sessionID: dbConfig().doc(`/${RESTAURANTS}/${orderDetails.restaurantID}/${SESSIONS}/${sessionID}`),
         tableID: dbConfig().doc(`/${RESTAURANTS}/${orderDetails.restaurantID}/${TABLES}/${tableID}`),
         start_timestamp: admin.firestore.Timestamp.fromDate(new Date()),
