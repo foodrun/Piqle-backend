ff5408823dedb3885eb0ce9b22c0ef3cb5b74386
Hrishikesh Kakkad <hrishidkakkad@outlook.com>
2021-07-13 20:14:29 +0530

    Bill details: Completed master object creation - TDD

--- src/services/RestaurantService/BillingService/billing.ts
+++ src/services/RestaurantService/BillingService/billing.ts
@@ -5,23 +5,45 @@ interface IOrderBill {
   item_name: string;
   item_quantity: number;
   item_price: number;
-  item_total: number;
+  item_total: number | null;
   placed_by: string;
   orderID: string;
   orderStatus: string;
 }
 export class BillingService {
-  getItemsArrayFromOrder(order: INewOrder, orderID: string): Array<IOrderBill> {
-    return [
-      {
-        item_name: 'chicken',
-        item_quantity: 5,
-        item_price: 50,
-        item_total: 250,
-        placed_by: 'abcdef',
-        orderID: orderID,
-        orderStatus: 'delivered',
-      },
-    ];
+  getItemsArrayFromOrder(orderDetails: INewOrder, orderID: string): Array<IOrderBill> {
+    const orderItemList: Array<IOrderBill> = [];
+    orderDetails.order.forEach(item => {
+      if (item.customizable.length < 1) {
+        const itemGenerator = {} as IOrderBill;
+        itemGenerator['item_name'] = item.title;
+        itemGenerator['item_price'] = item.price;
+        itemGenerator['item_quantity'] = item.quantity;
+        itemGenerator['orderID'] = orderID;
+        itemGenerator['orderStatus'] = orderDetails.orderStatus;
+        itemGenerator['placed_by'] = orderDetails['memberID'];
+        itemGenerator['item_total'] = orderDetails.orderStatus === 'delivered' ? item.quantity * item.price : null;
+        orderItemList.push(itemGenerator);
+      } else {
+        item.customizable.forEach(itemCustomization => {
+          if (itemCustomization.optionQuantity > 0) {
+            const itemGenerator = {} as IOrderBill;
+            itemGenerator['item_name'] = itemCustomization.optionTitle;
+            itemGenerator['item_price'] = itemCustomization.optionPrice;
+            itemGenerator['item_quantity'] = itemCustomization.optionQuantity;
+            itemGenerator['item_total'] =
+              orderDetails.orderStatus === 'delivered'
+                ? itemCustomization.optionPrice * itemCustomization.optionQuantity
+                : null;
+            itemGenerator['orderID'] = orderID;
+            itemGenerator['orderStatus'] = orderDetails.orderStatus;
+            itemGenerator['placed_by'] = orderDetails.memberID;
+            orderItemList.push(itemGenerator);
+          }
+        });
+      }
+    });
+    console.log(orderItemList, 'orderItemList');
+    return orderItemList;
   }
 }
